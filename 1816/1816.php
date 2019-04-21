09ded4f77c1237fd7ecd8713bbfc0863883f81f6
huynhchicuongz1 <huynhchicuongz1@gmail.com>
2019-04-21 16:41:27 +0700

    upload 18016
    upload 18016

---
+++ 1816/1816.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<?php
+    $url_host = 'http://' . $_SERVER['HTTP_HOST'];
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+    preg_match_all($pattern_uri, __DIR__, $matches);
+    $url_path = $url_host . $matches[1][0];
+    $url_path = str_replace('\\', '/', $url_path);
+
+    if (!class_exists('lessc')) {
+        $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+        require_once($dir_block . '/libs/lessc.inc.php');
+    }
+    $less = new lessc;
+    $less->compileFile('less/1816.less', 'css/1816.css');
+?>
+<html lang="zxx">
+    <head>
+        <title>1816</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/1816.css" rel="stylesheet" type="text/css"/>
+        <script src="<?php echo $url_path ?>/js/jquery-2.1.4.min.js"></script>
+        <script src="<?php echo $url_path ?>/js/bootstrap.min.js"></script>
+        <script src="<?php echo $url_path ?>/js/1816.js"></script>
+    </head>
+    <body>
+        <?php include './1816-content.php'; ?>
+    </body>
+</html>
