0b3725c7ea6623c6e77c7950b9c50905012c2ec0
Thai Nguyen <vanthai100298@gmail.com>
2019-05-03 19:43:46 +0700

    XONG GALLER VS PROJECT
    XONG GALLER VS PROJECT

---
+++ PROJECTS/1804-content.php
@@ -0,0 +1,141 @@
+<?php
+$url_host = $_SERVER['HTTP_HOST'];
+
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+
+$url_path = $url_host . $matches[1][0];
+
+$url_path = str_replace('\\', '/', $url_path);
+?>
+
+<div class="type-1804">
+    <section>
+        <div class="row">
+            <div class="col-md-12">
+                <!-- Portfolio Filter -->
+                <div class="portfolio-filter">
+                    <a href="#" class="active" data-filter="*">All projects</a>
+                    <a href="#branding" class="" data-filter=".branding">Commercial</a>
+                    <a href="#design" class="" data-filter=".design">Hospitaly</a>
+                    <a href="#photography" class="" data-filter=".photography">Office</a>
+                    <a href="#photography" class="" data-filter=".photography">Residential</a>
+                </div>
+                <!-- End Portfolio Filter -->
+
+                <!-- Portfolio Gallery Grid -->
+                <div class="gallery-isotope" data-lightbox="gallery">
+
+                    <!-- Portfolio Item Start -->
+                    <div class="gallery-item design">
+                        <div class="thumb">
+                            <img class="img-fullwidth" src="<?php echo $url_path ?>/images/p1.jpg" alt="project">
+                            <div class="overlay-shade"></div>
+                            <div class="text-holder">
+                                <div class="title text-center">Eco Green Interior</div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Portfolio Item End -->
+
+                    <!-- Portfolio Item Start -->
+                    <div class="gallery-item branding photography">
+                        <div class="thumb">
+                            <img class="img-fullwidth" src="images/p2.jpg" alt="project">
+                            <div class="overlay-shade"></div>
+                            <div class="text-holder">
+                                <div class="title text-center">Modern Elegance Suite</div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Portfolio Item End -->
+
+                    <!-- Portfolio Item Start -->
+                    <div class="gallery-item branding">
+                        <div class="thumb">
+                            <img class="img-fullwidth" src="images/p3.jpg" alt="project">
+                            <div class="overlay-shade"></div>
+                            <div class="text-holder">
+                                <div class="title text-center">Apartment Renovation</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Portfolio Item End -->
+
+                    <!-- Portfolio Item Start -->
+                    <div class="gallery-item branding">
+                        <div class="thumb">
+                            <img class="img-fullwidth" src="images/p4.jpg" alt="project">
+                            <div class="overlay-shade"></div>
+                            <div class="text-holder">
+                                <div class="title text-center">Vimeo Video</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Portfolio Item End -->
+
+                    <!-- Portfolio Item Start -->
+                    <div class="gallery-item branding photography">
+                        <div class="thumb">
+                            <img class="img-fullwidth" src="images/p5.jpg" alt="project">
+                            <div class="overlay-shade"></div>
+                            <div class="text-holder">
+                                <div class="title text-center">Restaurant In Texas</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Portfolio Item End -->
+
+                    <!-- Portfolio Item Start -->
+                    <div class="gallery-item design">
+                        <div class="thumb">
+                            <img class="img-fullwidth" src="images/p6.jpg" alt="project">
+                            <div class="overlay-shade"></div>
+                            <div class="text-holder">
+                                <div class="title text-center">Summer House</div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Portfolio Item End -->
+
+                    <!-- Portfolio Item Start -->
+                    <div class="gallery-item design">
+                        <div class="thumb">
+                            <img class="img-fullwidth" src="images/p7.jpg" alt="project">
+                            <div class="overlay-shade"></div>
+                            <div class="text-holder">
+                                <div class="title text-center">Luxury Living Room</div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Portfolio Item End -->
+
+                    <!-- Portfolio Item Start -->
+                    <div class="gallery-item branding">
+                        <div class="thumb">
+                            <img class="img-fullwidth" src="images/p8.jpg" alt="project">
+                            <div class="overlay-shade"></div>
+                            <div class="text-holder">
+                                <div class="title text-center">Cozy Bedroom</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Portfolio Item End -->
+
+                </div>
+            </div>
+        </div>
+        <!-- view All project -->
+        <div class="view-all-project">
+            <a href="#">view all project</a>
+        </div>
+        <!-- view All project the end -->
+    </section>
+</div>
